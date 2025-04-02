81a473e191a3c068e887d0647f316de4153d3e82
Samy Touabi <stoua026@uottawa.ca>
2025-04-01 22:56:34 -0400

    added radio buttons for push ups and course

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Global element references
 var cats = document.getElementById("cats");
-
 var input_date = document.getElementById("date");
 Date.prototype.toDateInputValue = function () {
   var local = new Date(this);
@@ -10,96 +10,66 @@ input_date.value = new Date().toDateInputValue();
 var input_name = document.getElementById("name");
 var input_age = document.getElementById("age");
 var input_sexe = document.getElementById("sexe");
-
 var summary = document.getElementById("summary_full");
 var container_summary = document.getElementById("container_summary");
 
+// Define mutually exclusive groups as an array of arrays
+// Group 0: "Push genou" (default) and "Push up"
+// Group 1: "Cooper course" (default) and "Cooper nage"
 var mutuallyExclusiveGroups = [
-  ["Push genou", "Push up"], // First group
-  // You can add more groups later if needed
+  ["Push genou", "Push up"],
+  ["Cooper course", "Cooper nage"],
 ];
 
-// (async () => {
-//     const url = "https://raw.githubusercontent.com/kyocanada/check-my-workouts/master/indices.xlsx";
-//     const data = await (await fetch(url)).arrayBuffer();
-
-//     const workbook = XLSX.read(data);
-//     console.log("workbook.Sheets ", workbook.Sheets)
-
-//     const ws = XLSX.utils.sheet_to_json(workbook.Sheets.Homme)
-//     console.log(ws)
-
-//     var categories = []
-//     var exercices = []
-
-//     ws.forEach(r => {
-//         if (categories.indexOf(r.Categorie) === -1)
-//             categories.push(r.Categorie)
-//         if (exercices.indexOf(r.Exercice) === -1)
-//             exercices.push(r.Exercice)
-//     });
-//     console.log(categories)
-//     console.log(exercices)
-//     console.log(workbook.Sheets.Homme["A1"].h)
-//     var range = XLSX.utils.decode_range(workbook.Sheets.Homme['!ref'])
-//     var rowCount = range.e.r
-//     var colCount = range.e.c
-// })();
-
-var csv;
 var cat_exes = [];
+var csv;
 
+// Fetch and process CSV data
 JSC.fetch(
   "https://raw.githubusercontent.com/kyocanada/check-my-workouts/master/indices.csv"
 )
-  // JSC.fetch("./indices.csv")
-  .then((response) => response.text()) // promise
+  .then((response) => response.text())
   .then((text) => {
     csv = csvToJsonRegex(text);
-    // console.log("text", text)
-    // console.log("csv.json", csv)
-
     csv.forEach((line) => {
       var cat_exe = Object.create({
         Categorie: line.Categorie,
         Exercice: line.Exercice,
         Unite: line.Unite.trim(),
       });
-
-      // console.log("", `${cat_exe.Categorie} / ${cat_exe.Exercice} : ${isCatExeInArray(cat_exes, cat_exe)}`)
-
+      // For each mutually exclusive group, if the exercise is in that group, insert the option selector
+      for (let g = 0; g < mutuallyExclusiveGroups.length; g++) {
+        if (mutuallyExclusiveGroups[g].includes(cat_exe.Exercice)) {
+          insertExclusiveOptionSelector(line.Categorie, g);
+        }
+      }
+      // Determine where to insert this exercise in the dynamic structure
       switch (isCatExeInArray(cat_exes, cat_exe)) {
-        case 1: // Exercice et categorie existent deja
+        case 1:
           break;
-        case 2: // Nouvel exercice sous categorie existant
+        case 2:
           cat_exes.push(cat_exe);
-
           var catDiv = document.getElementById(
             `container_${encode(line.Categorie)}`
           );
-
           var exeDiv = createExe(cat_exe);
-
           catDiv.appendChild(exeDiv);
           break;
-        case 3: // Nouvelle catégorie et nouvel exercice
+        case 3:
           cat_exes.push(cat_exe);
-
+          // Create new category container
           var textCat = document.createElement("h2");
           textCat.setAttribute("class", "cat_title");
           textCat.innerHTML = `▷ ${line.Categorie}`;
           cats.appendChild(textCat);
-
           var catContainer = document.createElement("div");
           catContainer.setAttribute(
             "id",
             `container_${encode(line.Categorie)}`
           );
           catContainer.setAttribute("class", "cat hide");
-
           var exeDiv = createExe(cat_exe);
           catContainer.appendChild(exeDiv);
-
           textCat.addEventListener("click", function () {
             catContainer.classList.toggle("hide");
             var txtcat = catContainer.classList.contains("hide")
@@ -107,23 +77,84 @@ JSC.fetch(
               : `▽ ${line.Categorie}`;
             textCat.innerHTML = txtcat;
           });
-
           cats.appendChild(catContainer);
           break;
         default:
           break;
       }
     });
-
-    // console.log("cat-exes", cat_exes)
   });
 
+// Inserts an option selector into the category container for a given group
+function insertExclusiveOptionSelector(category, groupIndex) {
+  var containerId = "container_" + encode(category);
+  var catContainer = document.getElementById(containerId);
+  // Check if an option selector for this group is already inserted
+  if (
+    catContainer &&
+    !catContainer.querySelector(
+      ".option-selector[data-group='" + groupIndex + "']"
+    )
+  ) {
+    var optionDiv = document.createElement("div");
+    optionDiv.classList.add("option-selector");
+    optionDiv.setAttribute("data-group", groupIndex);
+    // Unique radio name per category and group
+    var radioName = "exclusiveOptions_" + encode(category) + "_" + groupIndex;
+    // Set the default checked attribute on the first option (i.e. mutuallyExclusiveGroups[groupIndex][0])
+    optionDiv.innerHTML = `<p class="option-title">Sélectionnez une option</p>
+      <div class="option-buttons">
+         <label class="option-label">
+             <input type="radio" name="${radioName}" value="${mutuallyExclusiveGroups[groupIndex][0]}" checked>
+             ${mutuallyExclusiveGroups[groupIndex][0]}
+         </label>
+         <label class="option-label">
+             <input type="radio" name="${radioName}" value="${mutuallyExclusiveGroups[groupIndex][1]}">
+             ${mutuallyExclusiveGroups[groupIndex][1]}
+         </label>
+      </div>`;
+    optionDiv.querySelectorAll('input[type="radio"]').forEach((radio) => {
+      radio.addEventListener("change", function () {
+        updateExclusiveDisplayForCategoryAndGroup(category, groupIndex);
+      });
+    });
+    // Insert the option selector at the top of the category container
+    catContainer.insertBefore(optionDiv, catContainer.firstChild);
+    // Update display based on the default checked option
+    updateExclusiveDisplayForCategoryAndGroup(category, groupIndex);
+  }
+}
+
+// Updates the display for a given category and group based on the selected radio button
+function updateExclusiveDisplayForCategoryAndGroup(category, groupIndex) {
+  var containerId = "container_" + encode(category);
+  var catContainer = document.getElementById(containerId);
+  var radioName = "exclusiveOptions_" + encode(category) + "_" + groupIndex;
+  var selectedRadio = catContainer.querySelector(
+    'input[name="' + radioName + '"]:checked'
+  );
+  var selectedValue = selectedRadio ? selectedRadio.value : null;
+  // For each exclusive exercise in this container for this group, show only if it matches the selected value
+  catContainer
+    .querySelectorAll(`.exe[data-exclusive='true'][data-group='${groupIndex}']`)
+    .forEach(function (exeDiv) {
+      if (
+        selectedValue &&
+        exeDiv.getAttribute("data-exercise") === selectedValue
+      ) {
+        exeDiv.style.display = "";
+      } else {
+        exeDiv.style.display = "none";
+      }
+    });
+}
+
+// Convert CSV string to JSON using regex
 function csvToJsonRegex(csvString) {
   const regex = /,(?=(?:[^"]*"[^"]*")*(?![^"]*"))/;
   const rows = csvString.split("\n");
   const headers = rows[0].split(regex);
   const jsonData = [];
-
   for (let i = 1; i < rows.length - 1; i++) {
     const values = rows[i].split(regex);
     const obj = {};
@@ -131,13 +162,12 @@ function csvToJsonRegex(csvString) {
       if (j < 5) {
         values[j] =
           String(values[j]).charAt(0).toUpperCase() +
-          String(values[j]).slice(1); // Capitalize first letter
+          String(values[j]).slice(1);
       }
       obj[headers[j].trim()] = values[j];
     }
     jsonData.push(obj);
   }
-
   return jsonData;
 }
 
@@ -149,62 +179,33 @@ function isCatExeInArray(array, cat_exe) {
   const foundCatExe = array.find(
     (e) => cat_exe.Categorie === e.Categorie && cat_exe.Exercice === e.Exercice
   );
-  if (typeof foundCatExe !== "undefined") return 1; // Exercice et categorie existent deja
+  if (typeof foundCatExe !== "undefined") return 1;
   const foundCat = array.find((e) => cat_exe.Categorie == e.Categorie);
-  if (typeof foundCat !== "undefined") return 2; // Nouvel exercice sous categorie existant
-  return 3; // Nouvelle catégorie et nouvel exercice
+  if (typeof foundCat !== "undefined") return 2;
+  return 3;
 }
 
+// Create the exercise element; if it belongs to a mutually exclusive group, mark it with data attributes
 function createExe(obj) {
   var exeDiv = document.createElement("div");
   exeDiv.setAttribute("id", encode(obj.Exercice));
   exeDiv.setAttribute("class", "exe");
-
-  // Check if this exercise is part of a mutually exclusive group
-  let isExclusiveExercise = false;
-  let groupIndex = -1;
-  let groupName = "";
-
-  for (let i = 0; i < mutuallyExclusiveGroups.length; i++) {
-    if (mutuallyExclusiveGroups[i].includes(obj.Exercice)) {
-      isExclusiveExercise = true;
-      groupIndex = i;
-      groupName = `exclusive_group_${i}`;
+  // Check if the exercise belongs to any mutually exclusive group
+  var groupIndex = -1;
+  for (let g = 0; g < mutuallyExclusiveGroups.length; g++) {
+    if (mutuallyExclusiveGroups[g].includes(obj.Exercice)) {
+      groupIndex = g;
       break;
     }
   }
-
-  // For exclusive exercises, add a radio button
-  if (isExclusiveExercise) {
-    var radioContainer = document.createElement("div");
-    radioContainer.setAttribute("class", "radio-container");
-
-    var radioInput = document.createElement("input");
-    radioInput.setAttribute("type", "radio");
-    radioInput.setAttribute("name", groupName);
-    radioInput.setAttribute("id", `radio_${encode(obj.Exercice)}`);
-    radioInput.setAttribute("value", obj.Exercice);
-
-    // Add event listener to the radio button
-    radioInput.addEventListener("change", function () {
-      // Enable inputs for this exercise
-      handleExclusiveSelection(groupIndex, obj.Exercice);
-    });
-
-    radioContainer.appendChild(radioInput);
-
-    var radioLabel = document.createElement("label");
-    radioLabel.setAttribute("for", `radio_${encode(obj.Exercice)}`);
-    radioLabel.innerText = "Select";
-    radioContainer.appendChild(radioLabel);
-
-    exeDiv.appendChild(radioContainer);
+  if (groupIndex !== -1) {
+    exeDiv.setAttribute("data-exclusive", "true");
+    exeDiv.setAttribute("data-group", groupIndex);
+    exeDiv.setAttribute("data-exercise", obj.Exercice);
   }
-
   var textExe = document.createElement("h3");
   textExe.innerHTML = obj.Exercice;
   exeDiv.appendChild(textExe);
-
   switch (obj.Unite) {
     case "sec":
       var inputMin = document.createElement("input");
@@ -212,38 +213,20 @@ function createExe(obj) {
       inputMin.setAttribute("id", `input_${encode(obj.Exercice)}_min`);
       inputMin.value = 0;
       inputMin.addEventListener("focus", () => inputMin.select());
-
-      // If it's an exclusive exercise, initially disable it
-      if (isExclusiveExercise) {
-        inputMin.setAttribute("disabled", "true");
-        inputMin.setAttribute("data-exclusive-group", groupIndex);
-      }
-
       exeDiv.appendChild(inputMin);
-
       var textMin = document.createElement("h4");
       textMin.innerHTML = "min";
       exeDiv.appendChild(textMin);
-
       var inputSec = document.createElement("input");
       inputSec.setAttribute("type", "number");
       inputSec.setAttribute("id", `input_${encode(obj.Exercice)}_sec`);
       inputSec.value = 0;
       inputSec.addEventListener("focus", () => inputSec.select());
-
-      // If it's an exclusive exercise, initially disable it
-      if (isExclusiveExercise) {
-        inputSec.setAttribute("disabled", "true");
-        inputSec.setAttribute("data-exclusive-group", groupIndex);
-      }
-
       exeDiv.appendChild(inputSec);
-
       var textSec = document.createElement("h4");
       textSec.innerHTML = "s";
       exeDiv.appendChild(textSec);
       break;
-
     case "rep":
     default:
       var input = document.createElement("input");
@@ -251,64 +234,30 @@ function createExe(obj) {
       input.setAttribute("id", `input_${encode(obj.Exercice)}`);
       input.value = 0;
       input.addEventListener("focus", () => input.select());
-
-      // If it's an exclusive exercise, initially disable it
-      if (isExclusiveExercise) {
-        input.setAttribute("disabled", "true");
-        input.setAttribute("data-exclusive-group", groupIndex);
-      }
-
       exeDiv.appendChild(input);
-
       var unite = document.createElement("h4");
       unite.innerHTML = obj.Unite;
       exeDiv.appendChild(unite);
       break;
   }
-
   return exeDiv;
-}
-
-// Add this new function to handle exclusive exercise selection
-function handleExclusiveSelection(groupIndex, selectedExercise) {
-  // Get all input elements with the data-exclusive-group attribute matching this group
-  const inputs = document.querySelectorAll(
-    `[data-exclusive-group="${groupIndex}"]`
-  );
-
-  inputs.forEach((input) => {
-    // Enable input for the selected exercise, disable for others
-    const inputId = input.id;
-    if (inputId.includes(encode(selectedExercise))) {
-      input.removeAttribute("disabled");
-    } else {
-      input.setAttribute("disabled", "true");
-      input.value = 0; // Reset value when disabled
-    }
-  });
 }
 
 function submit() {
   container_summary.innerHTML = "";
-
   var age = input_age.value;
   var sexe = input_sexe.checked ? "F" : "H";
-
   var textDate = document.createElement("p");
   textDate.innerHTML = `Sommaire du ${input_date.value}`;
   container_summary.appendChild(textDate);
-
   var textNom = document.createElement("p");
   textNom.innerHTML = `Pour ${input_name.value} (${age}) : ${sexe}`;
   container_summary.appendChild(textNom);
-
   var desc = "";
   var color = "";
-
   cat_exes.forEach((cat_exe) => {
     var exe_value = 0;
     var text_result = "";
-
     switch (cat_exe.Unite.trim()) {
       case "sec":
         var exe_value_min = parseInt(
@@ -318,7 +267,6 @@ function submit() {
           document.getElementById(`input_${encode(cat_exe.Exercice)}_sec`).value
         );
         exe_value = exe_value_min * 60 + exe_value_sec;
-
         text_result = `${cat_exe.Exercice}: ${exe_value_min}min ${exe_value_sec}s`;
         break;
       case "rep":
@@ -329,24 +277,20 @@ function submit() {
         text_result = `${cat_exe.Exercice}:  ${exe_value} ${cat_exe.Unite}`;
         break;
     }
-
     if (exe_value > 0) {
       var exeDiv = document.createElement("div");
       var textExe = document.createElement("h3");
       textExe.innerHTML = text_result;
       exeDiv.appendChild(textExe);
-
       const indices = csv.filter(
         (i) =>
           cat_exe.Categorie === i.Categorie &&
           cat_exe.Exercice === i.Exercice &&
           i.Sexe === sexe
       );
-
       if (indices.length > 0) {
         var textDesc = document.createElement("h3");
         var indice = indices[0];
-
         if (exe_value < indice.Orange) {
           desc = "Problématique";
           color = "red";
@@ -366,12 +310,10 @@ function submit() {
         textDesc.setAttribute("class", color);
         textDesc.innerHTML = desc;
         exeDiv.appendChild(textDesc);
-
         container_summary.appendChild(exeDiv);
       }
     }
   });
-
   summary.classList.remove("hide");
 }
 
@@ -379,11 +321,10 @@ function hideSummary() {
   summary.classList.add("hide");
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", function () {
   var elAd = document.getElementById("weebly-footer-signup-container-v3");
   if (elAd) {
     elAd.remove();
-
     var divs = document.getElementsByTagName("div");
     divs[divs.length - 2].remove();
   }
